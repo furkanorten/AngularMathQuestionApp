24349af51c8b7d28c6a2ec1f2cda621bc22c1083
furkanorten <60614040+furkanorten@users.noreply.github.com>
2022-08-22 07:09:22 +0300

    changing destination folder

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'
+
+import { AppComponent } from './app.component';
+import { EqualityComponent } from './equality/equality.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EqualityComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
